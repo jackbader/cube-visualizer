7440f75f7262673d92e4261f73ddb336b5b875eb
Jack Bader <jack@culpritunderwear.com>
2024-06-17 12:37:27 -0400

    rebuild

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-'use client'
+'use client';
 
-import { Canvas } from "@react-three/fiber";
-import styles from "./page.module.css";
-import ThreeScene from "./three-scene";
-
-
+import styles from './page.module.css';
+import ThreeScene from './three-scene';
 
 export default function Home() {
   return (
